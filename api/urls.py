b271c005313b4c448e22b31cd29ca62ca24b2133
FarmerJohnsBessie <cz52013141834@gmail.com>
2024-07-03 14:29:38 -0400

    Made some change. Added profile update function in views.py

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('questions/', views.get_random_questions, name='get_random_questions'),
     path('get_question/', views.get_question, name='get_question'),
     path('check_answer/', views.check_answer, name='check_answer'),
+    path('get_answer/', views.get_answer, name='get_answer'),
+
     path('profile/', views.profile_view, name='profile'),
+    path('profile/update_biography/', views.update_biography, name='update_biography'),
 
     path('login/', user_views.login_view, name='login'),
     path('logout/', user_views.logout_view, name='logout'),
