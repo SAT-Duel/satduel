d679e4ef674e6b809abdeb82e9680868a216f813
FarmerJohnsBessie <cz52013141834@gmail.com>
2024-10-08 19:09:11 -0700

    refactored views

--- satduel/urls.py
+++ satduel/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/', include('api.views.urls')),
+    path('api/', include('api.urls')),
 
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
